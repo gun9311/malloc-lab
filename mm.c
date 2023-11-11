ffaa00c179588edd1968a9cd61c023643e892178
Ubuntu <Park>
2023-11-11 06:03:25 +0000

    first

--- mm.c
+++ mm.c
@@ -36,21 +36,105 @@ team_t team = {
 };
 
 /* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+#define ALIGNMENT 8 // 더블 워드 정렬
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) // 올림하여 데이터 크기 정렬
 
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 'size_t' 데이터 타입의 크기를 정렬 // 여기서 'size_t' 는 'char'형
 
+#define WSIZE       4 // 워드 사이즈
+#define DSIZE       8 // 더블 워드 사이즈
+#define CHUNKSIZE   (1<<12) // 4KB로 초기 블록 설정
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y)) // 최댓값 찾기
+
+#define PACK(size, alloc) ((size) | (alloc)) // 사이즈와 할당여부를 패킹
+
+#define GET(p)          (*(unsigned int *)(p)) // p가 가르키는 값을 추출
+#define PUT(p, val)     (*(unsigned int *)(p) = (val)) // p가 가르키는 값을 val으로
+
+#define GET_SIZE(p)     (GET(p) & ~0x7) // 헤더의 값과 비트 연산하여 사이즈를 파악함.
+#define GET_ALLOC(p)    (GET(p) & 0x1)  // 할당 여부를 파악
+
+#define HDRP(bp)        ((char *)(bp) - WSIZE) // 해당 블록 헤드의 주소
+#define FTRP(bp)        ((char *)(bp) + GET_SIZE(HDRP(bp)) -DSIZE) // 해당 블록 풋터의 주소
+
+#define NEXT_BLKP(bp)   ((char *)(bp)) + GET_SIZE((((char *)(bp) - WSIZE))) // 다음 블록 메모리 시작 주소
+#define PREV_BLKP(bp)   ((char *)(bp)) - GET_SIZE((((char *)(bp) - DSIZE))) // 이전 블록 메모리 시작 주소
 /* 
  * mm_init - initialize the malloc package.
  */
-int mm_init(void)
+
+static void *coalesce(char *bp)
 {
-    return 0;
+    // 만약 확장 블럭 prev 블럭이 가용이면 연결하기
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))); // size_t는 바이트 사이즈 // prev 블럭 할당 여부 체크 // ((bp) - DSIZE) 로 prev 블럭 풋터 체크 불가??
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))); // next 블럭 할당 여부 체크
+    size_t size = GET_SIZE(HDRP(bp)); // 현재 블럭 사이즈
+
+    if (prev_alloc && next_alloc) { // Case 1 : prev, next 블록 모두 할당 상태이면
+        return bp; // 현재 주소 포인터 반환
+    }
+
+    else if (prev_alloc && !next_alloc) { // Case 2 : prev 블록은 할당 상태고, next 블록은 가용 상태이면
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))); // size = 현재 블록 사이즈 + next 블록 사이즈
+        PUT(HDRP(bp), PACK(size, 0)); // 연결된 현재 블록 기준으로 헤더 세팅
+        PUT(FTRP(bp), PACK(size, 0)); // 연결된 현재 블록 기준으로 풋터 세팅 // next 블록 기준으로 세팅하는 것도 상관없는지??
+    }
+
+    else if (!prev_alloc && next_alloc) { // Case 3 : prev 블록은 가용 상태고, next 블록은 할당 상태이면
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))); // size = 현재 블록 사이즈 + prev 블록 사이즈
+        PUT(FTRP(bp), PACK(size, 0)); // 연결된 블록에 풋터 세팅
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0)); // 연결된 prev 블록 기준으로 헤더 세팅
+        bp = PREV_BLKP(bp); //bp는 prev 블록 기준으로 갱신
+    }
+
+    else { // Case 4 : prev, next 블록 모두 가용 상태이면
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp))); // size = 현재 블록 사이즈 + prev 블록 사이즈 + next 블록 사이즈
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0)); // 연결된 prev 블록 기준으로 헤더 세팅
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0)); // 연결된 next 블록 기준으로 풋터 세팅 // 현재 블록 기준으로 세팅하는 것도 상관없는지??
+        bp = PREV_BLKP(bp); //bp는 앞 블록 기준으로 갱신
+    }
+    return bp; // 변경된 주소 포인터 반환
 }
+
+static void *extend_heap(size_t words) // 워드 사이즈로 인자 받음
+{   
+    char * bp;
+    size_t size;
+    
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE; // 홀짝 따져서 size = 바이트 사이즈로 저장
+    if ((long)(bp = mem_sbrk(size)) == -1) // mem_sbrk 후 old_brk를 반환받지 못하면 힙 확장 실패
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0)); // 프리 블록 헤더 값 넣기
+    PUT(FTRP(bp), PACK(size, 0)); // 프리 블록 풋터 값 넣기
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 에필로그 헤더 값 넣기
+
+    return coalesce(bp); // 이전 블록 가용이면 새 블록과 연결하기
+}
+
+static void* heap_listp ; // 힙 포인터 전역 변수 선언
+int mm_init(void)    
+{
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void*)-1) // 힙 생성 체크
+        return -1;
+    PUT(heap_listp, 0); // unused
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE,1)); // 프롤로그 블록 헤더 패킹
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE,1)); // 프롤로그 블록 풋터 패킹
+    PUT(heap_listp + (3*WSIZE), PACK(0,1)); // 에필로그 블록 헤더 패킹
+    heap_listp += (2*WSIZE); // 프롤로그 블록 풋터로 포인터 이동
+
+    extend_heap(CHUNKSIZE/WSIZE); // 워드 사이즈로 인자 넘김
+
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
+        return -1; // 실패하면
+    return 0; // 성공하면
+}
+
+
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -71,9 +155,15 @@ void *mm_malloc(size_t size)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
+
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
